04ac6a8060c6597e9ad259c42785dd9b73e5524a
Mihai <balea.mihai93@gmail.com>
2015-07-09 11:15:14 +0300

    deleted all

--- README.md
+++
@@ -1,3 +0,0 @@
-# hello-world
-
-version 2.0
--- Test.java
+++
@@ -1,9 +0,0 @@
-public class Test(){
-
-public static void main(String [] args){
-	int r=9;
-	System.out.println("This is just another test");
-	String maJoc
-
-}
-}
